da88d455bdcf35a89ef50ff1254c25dd1f87823b
Silvio Geoffroy <sageoffroy@gmail.com>
2018-04-23 23:44:46 -0300

    Configuracion style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 	
-//= require Chart.min
 //= require jquery
 //= require jquery_ujs
+//= require Chart.min
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
