71ea4f53a452da33dc265a679189a8748965dfe6
Silvio Geoffroy <sageoffroy@gmail.com>
2018-04-25 20:06:05 -0300

    Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :objetivo_mensuals
   resources :objetivo_semanals
   resources :carga_diaria
@@ -17,6 +18,8 @@ Rails.application.routes.draw do
   resources :tipo_documentos
   devise_for :users
   get 'home/index'
+  get 'admin', to: 'home#admin', as:'admin_index'
+
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
