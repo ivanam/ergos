7b3ac51939333a1c8ee73207294b6167d2dd6165
Silvio Geoffroy <sageoffroy@gmail.com>
2018-04-26 13:50:03 -0300

    Admin bloques

--- app/models/punto_ventum.rb
+++ app/models/punto_ventum.rb
@@ -1,2 +1,7 @@
 class PuntoVentum < ApplicationRecord
+  belongs_to :concesionaria, :foreign_key => 'concesionaria_id', :class_name => 'Concesionarium'
+
+  validates :concesionaria_id, :presence => { :message => "Debe completar la concesionaria donde pertenece el punto de venta" }
+  validates :nombre, :presence => { :message => "Debe completar el nombre del P. Venta" }
+  validates :domicilio, :presence => { :message => "Debe completar el  campo domicilio" }
 end
