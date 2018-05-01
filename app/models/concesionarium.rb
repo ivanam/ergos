b8df4fd4e139571e7b3979f4463035cbcc8faab3
martinnicolas <ttincho77@gmail.com>
2018-05-01 17:09:21 -0300

    Agrego validaciones a Concesionaria, cambio vistas, quito info que no se esta cargando

--- app/models/concesionarium.rb
+++ app/models/concesionarium.rb
@@ -2,6 +2,9 @@ class Concesionarium < ApplicationRecord
 	belongs_to :user
 	has_many :persona_concesionaria, :foreign_key => 'concesionaria_id', :class_name => 'PersonaConcesionarium'
 	has_many :personas, :through => :persona_concesionaria 
+
+	validates :nombre, :presence => { :message => "Debe completar el campo Nombre" }
+
 	accepts_nested_attributes_for :persona_concesionaria, allow_destroy: true
 
 	def to_s
