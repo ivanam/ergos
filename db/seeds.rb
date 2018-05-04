1f8403efc7d329ed8449f00790813526eaca8ca5
ivana <iva.moyano@gmail.com>
2018-05-04 12:12:13 -0300

    agrgeo cambios a show personas

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@
 # Tipo de documento
 # ===================
 
-TipoDocumento.create(descripcion: 'CUIT') #1
-TipoDocumento.create(descripcion: 'LE')#2
-TipoDocumento.create(descripcion: 'LC')#3
-TipoDocumento.create(descripcion: 'DNI')#4
-TipoDocumento.create(descripcion: 'CI')#5
+
+TipoDocumento.create(descripcion: 'LE')#1
+TipoDocumento.create(descripcion: 'LC')#2
+TipoDocumento.create(descripcion: 'DNI')#3
+TipoDocumento.create(descripcion: 'CI')#4
 
 # Tipo de objetivo
 # ===================
@@ -25,10 +25,10 @@ TipoObjetivo.create(descripcion: 'VENTAS')#2
 # Personas
 # ========
 
-Persona.create(tipo_documento_id: 1, numero_documento: 31343209,apellido: "Litterio", nombre: "Marcos", telefono: "0280154336704", email: "marcos0@gmail.com")
-Persona.create(tipo_documento_id: 1, numero_documento: 32343209,apellido: "Moyano", nombre: "Ivana", telefono: "0280154336457", email: "ivana@gmail.com")
-Persona.create(tipo_documento_id: 1, numero_documento: 33343209,apellido: "Pritzkler", nombre: "Gonzalo", telefono: "0280154336367", email: "gonzalo@gmail.com")
-Persona.create(tipo_documento_id: 1, numero_documento: 33343209,apellido: "Moreno", nombre: "Martin", telefono: "0280154336367", email: "martin@gmail.com")
+Persona.create(tipo_documento_id: 3, numero_documento: 31343209,apellido: "Litterio", nombre: "Marcos", telefono: "0280154336704", email: "marcos0@gmail.com")
+Persona.create(tipo_documento_id: 3, numero_documento: 32343209,apellido: "Moyano", nombre: "Ivana", telefono: "0280154336457", email: "ivana@gmail.com")
+Persona.create(tipo_documento_id: 3, numero_documento: 33343209,apellido: "Pritzkler", nombre: "Gonzalo", telefono: "0280154336367", email: "gonzalo@gmail.com")
+Persona.create(tipo_documento_id: 3, numero_documento: 33343209,apellido: "Moreno", nombre: "Martin", telefono: "0280154336367", email: "martin@gmail.com")
 
 
 # Usuarios
