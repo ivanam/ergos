d9c7beb5f6b6987540c8189dbca8512f891aa98d
Gonzalo <gjpritzker@gmail.com>
2018-05-12 14:23:03 -0300

    validaciones para objetivos mensuales

--- app/models/objetivo_mensual.rb
+++ app/models/objetivo_mensual.rb
@@ -10,4 +10,34 @@ class ObjetivoMensual < ApplicationRecord
 	#validates :vendedor_id, :presence => { :message => "Debe completar el campo Vendedor" }
 	validates :tipo_objetivo_id, :presence => { :message => "Debe completar el campo Objetivo Mensual" }
 
+	validate :validarCantidades
+
+end
+
+
+def validarCantidades
+    @obMen = ObjetivoMensual.where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where(vendedor_id: nil).first
+    @obmPv = ObjetivoMensual.select("sum(cantidad_propuesta) as cantidadPV", "id", "cantidad_propuesta", "punto_venta_id").where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where(vendedor_id: nil).first # Performs a COUNT(id)
+    @obmVend = ObjetivoMensual.select("sum(cantidad_propuesta) as cantidadVend", "id", "cantidad_propuesta", "vendedor_id").where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where.not(vendedor_id: nil).first
+    @obResto =  @obmPv.cantidadPV.to_i - @obmVend.cantidadVend.to_i
+    debugger
+    if (@obmPv.cantidad_propuesta != nil)
+         if (@obmPv.cantidadPV <= self.cantidad_propuesta.to_i)
+          errors.add(:base, 'No puede asignarle un numero de venta mayor al vendedor que al punto de venta')
+         end
+          if (self.cantidad_propuesta.to_i > @obResto)
+          	errors.add(:base, 'El valor del objetivo para el vendedor supera al mensual de vendedores, el valor esperado debe ser menor o igual a: '+@obResto.to_s+'')
+         end
+    end
+    if ((@obMen == nil) && (self.vendedor_id != nil))
+        errors.add(:base,'Primero debe crear un objetivo mensual para el punto de venta seleccionado')
+    end
+     debugger
+    if((self.vendedor_id == nil) && (@obMen != nil))
+          errors.add(:base,'Ya existe un Objetivo Mensual para ese mes y año para ese punto de venta')
+    end
+
+    if(self.vendedor_id == @obmVend.vendedor_id.to_i)
+          errors.add(:base,'Ya existe un Objetivo Mensual para ese mes y año para ese vendedor')
+    end 
 end
