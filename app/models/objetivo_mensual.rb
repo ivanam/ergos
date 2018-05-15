c0b00086c8b45014952c4810465c01d7c510b7d5
Gastón Díaz <gipdiaz@gmail.com>
2018-05-15 13:38:41 -0300

    Correccion en validacion de cantidad de objetivos mensuales

--- app/models/objetivo_mensual.rb
+++ app/models/objetivo_mensual.rb
@@ -38,20 +38,18 @@ class ObjetivoMensual < ApplicationRecord
     return total
   end
 
-end
 
-
-def validarCantidades
+  def validarCantidades
     @obMen = ObjetivoMensual.where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where(vendedor_id: nil).first
     @obmPv = ObjetivoMensual.select("sum(cantidad_propuesta) as cantidadPV", "id", "cantidad_propuesta", "punto_venta_id").where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where(vendedor_id: nil).first # Performs a COUNT(id)
     @obmVend = ObjetivoMensual.select("sum(cantidad_propuesta) as cantidadVend", "id", "cantidad_propuesta", "vendedor_id").where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where.not(vendedor_id: nil).first
     @obResto =  @obmPv.cantidadPV.to_i - @obmVend.cantidadVend.to_i
     if (@obmPv.cantidad_propuesta != nil)
-         if (@obmPv.cantidadPV <= self.cantidad_propuesta.to_i)
+         if (@obmPv.cantidadPV < self.cantidad_propuesta.to_i)
           errors.add(:base, 'No puede asignarle un numero de venta mayor al vendedor que al punto de venta')
          end
           if (self.cantidad_propuesta.to_i > @obResto)
-          	errors.add(:base, 'El valor del objetivo para el vendedor supera al mensual de vendedores, el valor esperado debe ser menor o igual a: '+@obResto.to_s+'')
+            errors.add(:base, 'El valor del objetivo para el vendedor supera al mensual de vendedores, el valor esperado debe ser menor o igual a: '+@obResto.to_s+'')
          end
     end
     if ((@obMen == nil) && (self.vendedor_id != nil))
@@ -66,3 +64,8 @@ def validarCantidades
           errors.add(:base,'Ya existe un Objetivo Mensual para ese mes y año para ese vendedor')
     end 
 end
+
+end
+
+
+
