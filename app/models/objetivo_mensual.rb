292c96015305e4bac51f31ad0b7d7cf84df10f21
Gonzalo <gjpritzker@gmail.com>
2018-05-17 12:48:56 -0300

    validaciones para objetivos mensuales y semanales

--- app/models/objetivo_mensual.rb
+++ app/models/objetivo_mensual.rb
@@ -2,8 +2,8 @@ class ObjetivoMensual < ApplicationRecord
 	belongs_to :punto_venta, :class_name => 'PuntoVentum', :foreign_key => 'punto_venta_id'
 	#belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
 
-    validates :mes, :presence => { :message => "Debe completar el campo Fecha de creacion" }
-    validates :anio, :presence => { :message => "Debe completar el campo Fecha de creacion" }
+  validates :mes, :presence => { :message => "Debe completar el campo Fecha de creacion" }
+  validates :anio, :presence => { :message => "Debe completar el campo Fecha de creacion" }
 	validates :cantidad_propuesta, :presence => { :message => "Debe completar el campo Cantidad" }
 	validates :cantidad_propuesta, numericality: { only_integer: true, :message => "El campo Cantidad debe ser un valor entero"}	
 	validates :punto_venta_id, :presence => { :message => "Debe completar el campo Punto de venta" }
