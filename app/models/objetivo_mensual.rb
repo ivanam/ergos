f9f8495f86f909c10f73915d5557297134a84fa6
Gonzalo <gjpritzker@gmail.com>
2018-05-18 17:31:56 -0300

    cambios en validacion de ob mensuales

--- app/models/objetivo_mensual.rb
+++ app/models/objetivo_mensual.rb
@@ -48,7 +48,6 @@ class ObjetivoMensual < ApplicationRecord
     @obMen = ObjetivoMensual.where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where(vendedor_id: nil).first
     @obmPv = ObjetivoMensual.select("sum(cantidad_propuesta) as cantidadPV", "id", "cantidad_propuesta", "punto_venta_id").where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where(vendedor_id: nil).group("id").first # Performs a COUNT(id)
     @obmVend = ObjetivoMensual.select("sum(cantidad_propuesta) as cantidadVend", "id", "cantidad_propuesta", "vendedor_id").where(:punto_venta_id => self.punto_venta_id, :tipo_objetivo_id => self.tipo_objetivo_id, :mes  => self.mes ,:anio=> self.anio).where.not(vendedor_id: nil).group("id").first
-    debugger
     if (@obmVend != nil)
     @obResto =  @obmPv.cantidadPV.to_i - @obmVend.cantidadVend.to_i
     else
