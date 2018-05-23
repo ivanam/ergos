0a440a9aa498607f49b857b28238ab573d977b0a
Gonzalo <gjpritzker@gmail.com>
2018-05-23 08:58:55 -0300

    validaciones mensuales

--- app/models/objetivo_semanal.rb
+++ app/models/objetivo_semanal.rb
@@ -42,10 +42,12 @@ end
     if ((@obMen != nil) && (descpOb.descripcion != 'COMPROMISO DE VENTAS SEMANAL'))
     	@obMenCantProp = @obMen.cantidad_propuesta.to_i
          if (@obMen.cantidad_propuesta.to_i < self.cantidad_propuesta.to_i)
+          debugger
            errors.add(:base, 'EL objetivo semanal no puede superar al objetivo mensual del punto de venta, el valor maximo deberia ser: '+@obMenCantProp.to_s+' repartido en las semanas restantes')
          end
          if (@obsemPv != nil)
-    	    @obResto =   self.cantidad_propuesta.to_i - @obsemPv.cantidad_propuesta.to_i
+          debugger
+    	    @obResto =  @obsemPv.cantidad_propuesta.to_i - self.cantidad_propuesta.to_i
            if (@obResto < self.cantidad_propuesta.to_i)
               errors.add(:base, 'El objetivo para esa semana deberia ser como maximo: '+@obResto.to_s+'')	
            end 
