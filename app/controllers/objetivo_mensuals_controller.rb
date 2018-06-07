e11914d8c4a4d3c3302582a806c11953cf962c3e
martinnicolas <ttincho77@gmail.com>
2018-06-07 18:05:57 -0300

    Agrego funcionalidad a botones mas y menos y un poco de formato a errores

--- app/controllers/objetivo_mensuals_controller.rb
+++ app/controllers/objetivo_mensuals_controller.rb
@@ -97,7 +97,7 @@ class ObjetivoMensualsController < ApplicationController
       if objetivo_op.save        
         msj += ' Oportunidades '
       else
-        errores[:oportunidades] = objetivo_op.errors
+        errores[:oportunidades] = "Oportunidades: #{objetivo_op.errors.full_messages.first}"
       end
     end
     objetivo_pm = ObjetivoMensual.find_by(mes: mes, anio: anio, vendedor_id: vendedor_id, tipo_objetivo_id: 4)
@@ -106,7 +106,7 @@ class ObjetivoMensualsController < ApplicationController
       if objetivo_pm.save
         msj += ' Pruebas de manejo '
       else
-        errores[:pruebas_manejo] = objetivo_pm.errors
+        errores[:pruebas_manejo] = "Pruebas de manejo: #{objetivo_op.errors.full_messages.first}"
       end
     end
     objetivo_v = ObjetivoMensual.find_by(mes: mes, anio: anio, vendedor_id: vendedor_id, tipo_objetivo_id: 5)
@@ -115,7 +115,7 @@ class ObjetivoMensualsController < ApplicationController
       if objetivo_v.save
         msj += ' Ventas '
       else
-        errores[:ventas] = objetivo_v.errors
+        errores[:ventas] = "Ventas: #{objetivo_op.errors.full_messages.first}"
       end
     end
     objetivo_f = ObjetivoMensual.find_by(mes: mes, anio: anio, vendedor_id: vendedor_id, tipo_objetivo_id: 8)
@@ -124,7 +124,7 @@ class ObjetivoMensualsController < ApplicationController
       if objetivo_f.save
         msj += ' Financiaciones '
       else
-        errores[:financiaciones] = objetivo_f.errors
+        errores[:financiaciones] = "Financiaciones: #{objetivo_op.errors.full_messages.first}"
       end
     end
     objetivo_c = ObjetivoMensual.find_by(mes: mes, anio: anio, vendedor_id: vendedor_id, tipo_objetivo_id: 3)
@@ -133,7 +133,7 @@ class ObjetivoMensualsController < ApplicationController
       if objetivo_c.save
         msj += ' Calidad '
       else
-        errores[:calidad] = objetivo_c.errors
+        errores[:calidad] = "Calidad: #{objetivo_op.errors.full_messages.first}"
       end
     end
     if msj == ''
@@ -146,7 +146,7 @@ class ObjetivoMensualsController < ApplicationController
         format.html { redirect_to "/vendedors/#{vendedor_id}", notice: msj }
         format.json { head :no_content }
       else
-        format.html { redirect_to "/vendedors/#{vendedor_id}", notice: errores }
+        format.html { redirect_to "/vendedors/#{vendedor_id}", alert: errores }
         format.json { render json: errores, status: :unprocessable_entity }
       end
     end    
