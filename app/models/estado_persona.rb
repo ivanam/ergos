0017cb0a0eeef977b5aba3f0d997be30e4656c10
Gonzalo <gjpritzker@gmail.com>
2018-06-28 16:07:08 -0300

    cambios en inasistencias

--- app/models/estado_persona.rb
+++ app/models/estado_persona.rb
@@ -11,9 +11,12 @@ class EstadoPersona < ApplicationRecord
   validate  :fechaLic
 
   def fechaLic
-    agenteLic = EstadoPersona.where(:vendedor_id => self.vendedor_id).first
+    agenteLic = EstadoPersona.where(:vendedor_id => self.vendedor_id).last
+    agenteSelf = EstadoPersona.where(:vendedor_id => self.vendedor_id).where(id: self.id).first
     agenteLic1 = EstadoPersona.where(:vendedor_id => self.vendedor_id).where.not(id: self.id).first
-    if (agenteLic1 != nil)
+    debugger
+    if (agenteSelf == nil)
+     if (agenteLic1 != nil)
 	    if (agenteLic != nil)
 	      if (agenteLic.fecha_fin.to_i == nil)
 	    	 errors.add(:base, "Existe agente ya tiene una inasistencia sin fecha de finalización, debe finalizarla para poder generar otra inasistencia")
@@ -32,6 +35,10 @@ class EstadoPersona < ApplicationRecord
 	    	errors.add(:base, "La fecha de finalizacion es menor o igual a la  fecha de inicio de la última asistencia tomada")
 	      end
 
+	      if (agenteLic.fecha_fin.to_i >= self.fecha_fin.to_i)
+	    	errors.add(:base, "La fecha de finalizacion es menor o igual a la  fecha de inicio de la última asistencia tomada")
+	      end
+
 	      if (agenteLic.fecha_inicio.to_i == self.fecha_inicio.to_i) and (agenteLic.fecha_fin.to_i == self.fecha_fin.to_i)
 	    	 errors.add(:base, "Este vendedor ya tiene cargada una inasistencia en ese rango de fecha")
 	      end
@@ -39,6 +46,7 @@ class EstadoPersona < ApplicationRecord
 	  if (self.fecha_inicio.to_i > self.fecha_fin.to_i)
 	    	 errors.add(:base, "la fecha de inicio no puede ser mayor a la de fin")
 	  end
+	end
   	end
   end
 end
