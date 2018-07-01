22d15ca0cc6966c64cce24bfb1a9ca423c002690
martinnicolas <ttincho77@gmail.com>
2018-07-01 02:09:21 -0300

    Agrego busqueda de vendedores en inasistencias, correcciones para fecha de inicio y fin y detalles

--- app/models/estado_persona.rb
+++ app/models/estado_persona.rb
@@ -2,51 +2,47 @@ class EstadoPersona < ApplicationRecord
   #has_many :persona
   belongs_to :estado, :class_name => 'Estado', :foreign_key => 'estado_id' , optional: :true
   belongs_to :vendedor, optional: :true
-
-  validates :fecha_inicio, :presence => { :message => "Debe completar el campo Fecha Inicio" }
-  validates :fecha_fin, :presence => { :message => "Debe completar el campo Fecha Finalización" }
+  
   validates :estado, :presence => { :message => "Debe seleccionar el Estado" }
   validates :vendedor, :presence => { :message => "Debe seleccionar el Vendedor" }
+  validates :fecha_inicio, :presence => { :message => "Debe completar el campo Fecha de inicio" }
+  validates :fecha_fin, :presence => { :message => "Debe completar el campo Fecha de finalización" }
 
   validate  :fechaLic
 
   def fechaLic
   	hayCarga = CargaDiarium.where('fecha >= ?', self.fecha_inicio).where('fecha <= ?', self.fecha_fin).where(vendedor_id: self.vendedor_id).count 
-    agenteLic = EstadoPersona.where(:vendedor_id => self.vendedor_id).last
-    agenteSelf = EstadoPersona.where(:vendedor_id => self.vendedor_id).where(id: self.id).first
-    agenteLic1 = EstadoPersona.where(:vendedor_id => self.vendedor_id).where.not(id: self.id).first
-    if (agenteSelf == nil)
-     if (agenteLic1 != nil)
-	    if (agenteLic != nil)
-	      if (agenteLic.fecha_fin.to_i == nil)
-	    	 errors.add(:base, "Existe agente ya tiene una inasistencia sin fecha de finalización, debe finalizarla para poder generar otra inasistencia")
-	      end
-	    
-	      if (agenteLic.fecha_inicio.to_i >= self.fecha_inicio.to_i)
-	    	errors.add(:base, "La fecha de incio es menor o igual a la fecha de incio de la última asistencia tomada")
-	      end
-	    
-	      if (agenteLic.fecha_fin.to_i >= self.fecha_inicio.to_i)
-	    	errors.add(:base, "La fecha de incio es menor o igual a la  fecha de finalizacion de la última asistencia tomada")
-	      end
+    vendedorLic = EstadoPersona.where(:vendedor_id => self.vendedor_id).last
+    vendedorSelf = EstadoPersona.where(:vendedor_id => self.vendedor_id).where(id: self.id).first
+    vendedorLic1 = EstadoPersona.where(:vendedor_id => self.vendedor_id).where.not(id: self.id).first
+    if (vendedorSelf == nil)
+    	if (vendedorLic1 != nil)
+		    if (vendedorLic != nil)	    
+		      if (vendedorLic.fecha_inicio.to_i >= self.fecha_inicio.to_i)
+		    	errors.add(:base, "La fecha de incio es menor o igual a la fecha de incio de la última asistencia tomada")
+		      end
+		    
+		      if (vendedorLic.fecha_fin.to_i >= self.fecha_inicio.to_i)
+		    	errors.add(:base, "La fecha de incio es menor o igual a la fecha de finalizacion de la última asistencia tomada")
+		      end
 
 
-	      if (agenteLic.fecha_inicio.to_i >= self.fecha_fin.to_i)
-	    	errors.add(:base, "La fecha de finalizacion es menor o igual a la  fecha de inicio de la última asistencia tomada")
-	      end
+		      if (vendedorLic.fecha_inicio.to_i >= self.fecha_fin.to_i)
+		    	errors.add(:base, "La fecha de finalizacion es menor o igual a la fecha de inicio de la última asistencia tomada")
+		      end
 
-	      if (agenteLic.fecha_fin.to_i >= self.fecha_fin.to_i)
-	    	errors.add(:base, "La fecha de finalizacion es menor o igual a la  fecha de inicio de la última asistencia tomada")
-	      end
+		      if (vendedorLic.fecha_fin.to_i >= self.fecha_fin.to_i)
+		    	errors.add(:base, "La fecha de finalizacion es menor o igual a la fecha de inicio de la última asistencia tomada")
+		      end
 
-	      if (agenteLic.fecha_inicio.to_i == self.fecha_inicio.to_i) and (agenteLic.fecha_fin.to_i == self.fecha_fin.to_i)
-	    	 errors.add(:base, "Este vendedor ya tiene cargada una inasistencia en ese rango de fecha")
-	      end
-	    end
-	  if (self.fecha_inicio.to_i > self.fecha_fin.to_i)
-	    	 errors.add(:base, "la fecha de inicio no puede ser mayor a la de fin")
-	  end
-	 end
+		      if (vendedorLic.fecha_inicio.to_i == self.fecha_inicio.to_i) and (vendedorLic.fecha_fin.to_i == self.fecha_fin.to_i)
+		      	errors.add(:base, "Este vendedor ya tiene cargada una inasistencia en ese rango de fecha")
+		      end
+		    end
+		  	if (self.fecha_inicio.to_i > self.fecha_fin.to_i)
+		    	errors.add(:base, "la fecha de inicio no puede ser mayor a la de fin")
+		  	end
+		end
   	end
   	if (hayCarga != 0)
 	  	errors.add(:base,  "No se puede crear el registro de inasistencia retroactivo porque el vendedor ha cargado datos en ese período")
